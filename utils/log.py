8cdc567792882ca9d35dcfe70155475c7fff23eb
Bartlomiej Janiszewski <43078086+janiszewskibartlomiej@users.noreply.github.com>
2022-04-13 10:57:08 +0200

    added refresh method

--- utils/log.py
+++ utils/log.py
@@ -7,6 +7,12 @@ from utils.paths_builder import logs_file_path
 class Logger:
     _logger = None
 
+    @staticmethod
+    def get_logger():
+        if Logger._logger is None:
+            Logger()
+        return Logger._logger
+
     def __init__(self):
         if Logger._logger is not None:
             raise Exception("Singleton class - single instance only!")
@@ -14,17 +20,24 @@ class Logger:
             self.setup_logger("sms_logger")
 
     @staticmethod
-    def get_logger():
-        if Logger._logger is None:
-            Logger()
-        return Logger._logger
+    def refresh_log_handlers(logger):
+        formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m-%d %H:%M:%S')
 
-    @staticmethod
-    def setup_logger(name):
-        formatter = logging.Formatter(
-            fmt="%(asctime)s - %(levelname)s - %(message)s", datefmt="%m-%d %H:%M:%S"
-        )
-        file_handler = logging.FileHandler(logs_file_path())
+        file_handler = logging.FileHandler(test_logs_file_path())
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.DEBUG)
+        console_handler = logging.StreamHandler(stream=stdout)
+        console_handler.setFormatter(formatter)
+        console_handler.setLevel(logging.DEBUG)
+        for hdlr in logger.handlers[:]:  # remove all old handlers
+                logger.removeHandler(hdlr)
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    def setup_logger(self, name):
+        formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m-%d %H:%M:%S')
+
+        file_handler = logging.FileHandler(test_logs_file_path())
         file_handler.setFormatter(formatter)
         file_handler.setLevel(logging.DEBUG)
         logger = logging.getLogger(name)
